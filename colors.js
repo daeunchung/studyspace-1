a43e0f3c51614f5f247033232558d115abd0fec5
daeunchung <46639550+daeunchung@users.noreply.github.com>
2020-09-07 16:17:14 +0900

    Add jQuery with Google CDN

--- colors.js
+++ colors.js
@@ -1,20 +1,25 @@
 var Links = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i++;
-    }
+    // 이 웹페이지에 있는 모든 a tag를 jquery로 제어하겠다 -> $
+    $('a').css('color', color);
+
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i++;
+    // }
   }
 }
 // 객제의 프로퍼티, 메서드 끼리 구분하기위해 콤마(,) 필수
 var Body = {
   setColor:function (color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackgroundColor:function(color) {
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
 
