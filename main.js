b96a9bb906749e962d44e30beec613205b804f35
daeunchung <skyblue5023@naver.com>
2020-09-15 01:34:36 +0900

    DBconnection delete comments

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var mysql = require('mysql');
-// const { brotliDecompress } = require('zlib');
 var db = mysql.createConnection({
   host     : 'localhost',
   port     : '3307',
@@ -24,19 +23,6 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        /*
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-        */
         db.query(`SELECT * FROM topic`, function(error, topics){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
@@ -49,30 +35,6 @@ var app = http.createServer(function(request,response){
           response.end(html);         
         });
       } else {
-        /*
-        fs.readdir('./data', function(error, filelist){
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-        */
        db.query(`SELECT * FROM topic`, function(error, topics){
          if(error){
            throw error;
@@ -99,24 +61,6 @@ var app = http.createServer(function(request,response){
        });
       }
     } else if(pathname === '/create'){
-      /*fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `, '');
-        response.writeHead(200);
-        response.end(html);
-      });*/
-
       db.query(`SELECT * FROM topic`, function(error, topics){
         var title = 'Create';
         var list = template.list(topics);
@@ -143,13 +87,6 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          // var title = post.title;
-          // var description = post.description;
-          /*fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })*/
-
           db.query(`
           INSERT INTO topic (title, description, created, author_id) 
           VALUES(?, ?, NOW(), ?)`,
@@ -164,9 +101,6 @@ var app = http.createServer(function(request,response){
         )
       });
     } else if(pathname === '/update'){
-      // fs.readdir('./data', function(error, filelist){
-        // var filteredId = path.parse(queryData.id).base;
-        // fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
       db.query(`SELECT * FROM topic`, function(error, topics){
         if(error){
           throw error;
@@ -206,17 +140,6 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {Location: `/?id=${post.id}`});
             response.end(); 
           })
-          /*
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
-          */
       });
     } else if(pathname === '/delete_process'){
       var body = '';
@@ -232,14 +155,6 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {Location: `/`});
             response.end();
           })
-          /*
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-          */
       });
     } else {
       response.writeHead(404);
