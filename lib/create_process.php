7e0cae3473c6e6c42dd223b33e85f5a22ac53880
daeunchung <skyblue5023@naver.com>
2020-09-21 19:53:41 +0900

    last commit

--- lib/create_process.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents('../data/'.$_POST['title'],$_POST['description']);
-header('Location: /lib/index.php?id='.$_POST['title']);
-?>
--- lib/delete_process.php
+++
@@ -1,4 +0,0 @@
-<?php
-unlink('../data/'.$_POST['id']);
-header('Location: /lib/index.php');
-?>
