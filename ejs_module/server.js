db0aa5202252807303d5bfa6c378ae0d14ca3be0
daeunchung <skyblue5023@naver.com>
2020-09-30 13:30:51 +0900

    npm ejs

---
+++ ejs_module/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+var ejs = require('ejs');
+
+http.createServer(function(req, res){
+    fs.readFile('page.ejs', 'utf8', function(err, data){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        // 불러온 .ejs 파일을 render 메소드에 넘김, .ejs 파일을 분석하여 .html 파일로 만듬
+        res.end(ejs,render(data));
+    });
+}).listen(4008, function(){
+    console.log('server running at http://localhost:4008');
+});
